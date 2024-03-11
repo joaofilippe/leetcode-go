22bf3137a0c4b983aeb3c27f066eeae7e03a795c
João Filippe Rossi Rodrigues <joaofilippe@outlook.com>
2024-03-11 12:52:16 -0300

    better solution

--- 0014/longest_prefix.go
+++ 0014/longest_prefix.go
@@ -1,15 +1,11 @@
 package longest
 
 func longestCommonPrefix(strs []string) string {
-	if len(strs) == 1 { // handle only 1 element
-        return strs[0]
-    }
-    
-    p := strs[0]
-	var i int
-    for _, s := range strs {
-        for i = 0 ; i < len(s) && i < len(p) && p[i] == s[i]; i++ {}
-        p = p[:i]
-    }
-    return p
+	for i := 0; ; i++ {
+		for _, str := range strs {
+			if i == len(str) || str[i] != strs[0][i] {
+				return strs[0][:i]
+			}
+		}
+	}
 }
